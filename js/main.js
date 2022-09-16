b6ef1ac35f587be5bf2f5edf147293d77a82aeb8
alexanderh26 <hol14030@byui.edu>
2022-09-16 17:24:13 -0600

    wdd230

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 const today= new Date();
     document.querySelector("footer div span").textContent = today.getFullYear();
     document.getElementById("lastupdatedate").textContent = document.lastModified;
-    //asdf
+    
