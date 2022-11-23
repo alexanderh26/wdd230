ba7e3c808715876ca45ca2b98cf9dc59f808572c
alexanderh26 <hol14030@byui.edu>
2022-11-23 14:25:23 -0700

    asdf

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -34,4 +34,13 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+let lastVisited = window.localStorage.getItem("lastVisited");
+let lastVisitedDisplay = document.querySelector("#lastVisited");
+if (lastVisited) {
+    let days = Math.round((Date.now() - lastVisited));
+    lastVisitedDisplay.textContent = `You visited this page ${days} days ago.`;
+} else {
+    lastVisitedDisplay.textContent = `You visited this page for the first time today.`;
+}
     
