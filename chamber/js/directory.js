2ce2b8563760cc3084f9ecbf8bea6786f4484d24
alexanderh26 <hol14030@byui.edu>
2022-11-22 17:56:40 -0700

    directory

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,1 +1,23 @@
-const requestURL = "https://alexanderh26.github/chamber/data.json"
+const requestURL = "/Users/alexanderholloway/Desktop/School stuff/WDD230/wdd230-1/chamber/data.json"
+function displayBusinesses(business){
+    let card = document.createElement ('section');
+    let h2 = document.createElement('h2');
+    h2.textContent = business.name;
+    card.appendChild(h2);
+    document.querySelector('div.cards').appendChild(card);
+}
+fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (jsonObject){
+        console.table(jsonObject);
+        const businesses = jsonObject['businesses'];
+        businesses.forEach(displayBusinesses);
+
+
+
+
+    });
+    
+   
